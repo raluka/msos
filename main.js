8bf209657c47f3720cf1ec34760e58ea72b0356e
raluka <badoi.raluca@gmail.com>
2016-07-19 14:42:55 +0200

    create desktop app with electron after tutorialzine tutorial

---
+++ main.js
@@ -0,0 +1,37 @@
+// The most significant thing to note in package.json file is the scripts property, where we have ve defined the npm start command, allowing us
+// to run the app like we did earlier. When we call it, we ask electron to run the main.js file. This JS file contains a
+// short script that opens the app window, and defines some options and event handlers.
+
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 1000, height: 625});
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
