c074e26c205ec64ebbf0d6571f04a13d5243e539
raluka <badoi.raluca@gmail.com>
2016-07-19 18:40:44 +0200

    add auto-updater v0.0.2

--- README.md
+++ README.md
@@ -3,19 +3,22 @@
 To try Electron for the first time, I followed this [tutorial](http://tutorialzine.com/2015/12/creating-your-first-desktop-app-with-html-js-and-electron/).
 ## Running the App
 
+### Development pre-requirements:
+
 Since an Electron app is just a fancy Node.js app, you will need to have `npm` installed. [`npm`](https://docs.npmjs.com/getting-started/what-is-npm) is the package manager for JavaScript, and it is installed as part of `node`:
 - Install `node`:
 ```
 brew install node
 ```
-Install `wine` and `cask`:
+Install `wine` and `Xquartz`:
 ```
 brew install Caskroom/cask/xquartz
 brew install wine
 ```
-[`wine`](https://www.davidbaumgold.com/tutorials/wine-mac/) lets you run Windows apps without the Windows operating system.
+[`Wine`](https://www.davidbaumgold.com/tutorials/wine-mac/) lets you run Windows apps without the Windows operating system.
+[`Xquartz`](https://en.wikipedia.org/wiki/XQuartz) is an open source version of X Window System for Mac OS (a standard framework for creating applications with a graphical user interface on Unix-like operating systems).
 
-In case you fails to install wine, try install it without dependencies:
+In case you fails to install `wine`, try install it without dependencies:
 ```
 brew install --ignore-dependencies wine
 ```
@@ -23,6 +26,8 @@ and then install each dependency one at a time `brew install {dependency}`. The 
 ```
 libusb-compat, fontconfig, libtiff, webp, gd, libgphoto2, little-cms2, jasper, libicns, makedepend, openssl, sane-backends, libtasn1, and gnutls
 ```
+---
+
 Once you’ve got that covered, open a new cmd or terminal in the directory with the extracted files and run this command:
 ```
 npm install
@@ -40,9 +45,31 @@ Install [`electron-builder`](https://www.npmjs.com/package/electron-builder):
 ```
 npm install electron-builder --save-dev
 ```
-In order to generate packages, run this command in `/app/node_modules/.bin` folder:
+In order to generate packages, run this command in `/node_modules/.bin` folder:
 ```
 npm run dist
 ```
 
 All of your generated packages are available in `dist` folder. Although this will generate you files which you can install on Windows and OS X, we need to add some code to our app to integrate fetching and installing automatic updates.
+
+## Set auto-updates
+See prerelease - v0.0.2 for code.
+
+One extra thing you'll need to do is sign your application. You don't need to do it, but if you don't sign it, auto-updating mechanism won't work.
+You can create your own certificate for testing purposes by Keychain Access.
+TODO: Add how to create certificate info
+
+After you have this certificate, in your app directory, run:
+```
+export CSC_NAME="Certificate Name"
+```
+
+Electron is using Squirrel.Mac to auto-update your application. Auto-updating with Squirrel consists of two parts: requesting Squirrel package from your application and serving it on server.
+I followed [this](http://electron.rocks/publishing-for-os-x/) tutorial to set `auto-updater`
+
+Squirrel for Mac is expecting server to respond with 200 in JSON format if update is needed, or with 204 if app doesn't need to update.
+
+Next step is to generate versioned packages again from /app/node_modules/.bin folder:
+````
+npm run dist
+````
--- app/main.js
+++ app/main.js
@@ -1,37 +1,109 @@
-// The most significant thing to note in package.json file is the scripts property, where we have ve defined the npm start command, allowing us
-// to run the app like we did earlier. When we call it, we ask electron to run the main.js file. This JS file contains a
-// short script that opens the app window, and defines some options and event handlers.
+// Handle Squirrel events for Windows immediately on start
+if(require('electron-squirrel-startup')) return;
 
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
+const electron = require('electron');
+const {app} = electron;
+const {BrowserWindow} = electron;
+const {autoUpdater} = electron;
+const {ipcMain} = electron;
+const autoUpdater = require('auto-updater');
+const os = require('os');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const logger = require('winston');
+logger.level = 'debug';
+global.logger = logger;
+
+// Keep reference of main window because of GC
 var mainWindow = null;
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
-    app.quit();
+var updateFeed = 'http://localhost:3000/updates/latest';
+var isDevelopment = process.env.NODE_ENV === 'development';
+var feedURL = "";
+
+// Don't use auto-updater if we are in development
+if (!isDevelopment) {
+  // TODO: Add link to server_url/mac_updates/latest
+  if (os.platform() === 'darwin') {
+    updateFeed = 'http://localhost:3000/updates/latest';
   }
+  // TODO: Add link to server_url/windows_updates/latest
+  else if (os.platform() === 'win32') {
+    updateFeed = 'http://localhost:3000/latest/win' + (os.arch() === 'x64' ? '64' : '32');
+  }
+
+  autoUpdater.addListener("update-available", function(event) {
+    logger.debug("A new update is available");
+    if (mainWindow) {
+      mainWindow.webContents.send('update-message', 'update-available');
+    }
+  });
+  autoUpdater.addListener("update-downloaded", function(event, releaseNotes, releaseName, releaseDate, updateURL) {
+    logger.debug("A new update is ready to install", "Version ${releaseName} is downloaded and will be automatically installed on Quit");
+    if (mainWindow) {
+      mainWindow.webContents.send('update-message', 'update-downloaded');
+    }
+  });
+  autoUpdater.addListener("error", function(error) {
+    logger.error(error);
+    if (mainWindow) {
+      mainWindow.webContents.send('update-message', 'update-error');
+    }
+  });
+  autoUpdater.addListener("checking-for-update", function(event) {
+    logger.debug("Checking for update");
+    if (mainWindow) {
+      mainWindow.webContents.send('update-message', 'checking-for-update');
+    }
+  });
+  autoUpdater.addListener("update-not-available", function() {
+    logger.debug("Update not available");
+    if (mainWindow) {
+      mainWindow.webContents.send('update-message', 'update-not-available');
+    }
+  });
+
+  const appVersion = require('./package.json').version;
+  const feedURL = updateFeed + '?v=' + appVersion;
+  autoUpdater.setFeedURL(feedURL);
+}
+
+// Quit when all windows are closed
+app.on('window-all-closed', function() {
+  app.quit();
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
+// When application is ready, create application window
 app.on('ready', function() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 625});
 
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  logger.debug("Starting application");
 
-  // Emitted when the window is closed.
+  // Create main window
+  // Other options available at:
+  // http://electron.atom.io/docs/latest/api/browser-window/#new-browserwindow-options
+  mainWindow = new BrowserWindow({
+    name: "MSOS",
+    width: 400,
+    height: 600,
+    toolbar: false
+  });
+
+  // Target HTML file which will be opened in window
+  // TODO: Change here with index file from web app
+  mainWindow.loadURL('file://' + __dirname + "/index.html");
+
+  // Uncomment to use Chrome developer tools
+  // mainWindow.webContents.openDevTools({detach:true});
+
+  // Cleanup when window is closed
   mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
   });
+
+  if (!isDevelopment) {
+    mainWindow.webContents.on('did-frame-finish-load', function() {
+      logger.debug("Checking for updates: " + feedURL);
+      autoUpdater.checkForUpdates();
+    });
+  }
+
 });
